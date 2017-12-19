1db1dcd9cbf96e316c0a3b4f05a90e570a6348c7
kelichau <tchau2@outlook.com>
2017-12-18 20:55:03 -0500

    Adds Twitter keys to keys.js

---
+++ keys.js
@@ -0,0 +1,7 @@
+exports.twitterKeys = {
+  consumer_key: '3NF8wkRdPXVOBwBfZRsfI5148',
+  consumer_secret: 'OZ7WC1kNT24GdYqpU8V4T9Oj0a1TUe2fGZqGpFkrACYe2fG2Eg',
+  access_token_key: '794827563210113024-cb0UGQQZedpw57uzFD25pCN76fa98pl',
+  access_token_secret: 'Z00Q0cQ7FZrGzKklZlHYXFR0wqsLyQItEYHbFDHm3wqrg',
+};
+
