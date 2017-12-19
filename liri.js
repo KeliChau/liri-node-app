95003aa8ca9dcfaf3e4dbefd7c0427d21dc609fa
kelichau <tchau2@outlook.com>
2017-12-18 20:47:18 -0500

    Adds Spotify if statement

--- liri.js
+++ liri.js
@@ -47,3 +47,32 @@ if(argument === "movie-this"){
 	outputText();
 };
 
+// Spotify if statement
+if(argument === "spotify-this-song"){
+	var songTitle = process.argv[3];
+	spotify.search({type: 'track', query: songTitle}, function(error, data){
+		if(process.argv[3]){
+			var data = data.tracks.items;
+			for (var i = 0; i < data.length; i++){
+				console.log(data[i].name);
+				console.log(data[i].album.href);
+				console.log(data[i].album.name);
+				console.log(data[i].preview_url);
+
+				for(var j = 0; j < data[i].artists.length; j++){
+					console.log(data[i].artists[j].name);
+				}
+			}
+		}else{
+			spotify.search({type: 'track', query: "the sign"}, function(error, data){
+				var data = data.track.items;
+				console.log(data[0].name);
+				console.log(data[0].album.href);
+				console.log(data[0].album.name);
+				console.log(data[0].preview_url);
+				console.log(data[0].artists[0].name);
+			});
+		}
+	});
+	outputText();
+};
