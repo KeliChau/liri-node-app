7d6fb39df557e5b147148ec3f443d02042d62ef9
kelichau <tchau2@outlook.com>
2017-12-18 20:37:16 -0500

    Adds twitter if statement

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ var request = require(request);
 var fs = require(fs);
 var Spotify = require(spotify)
 var Twitter = require(twitter);
-var keys = require(./keys.js);
+var keys = require(keys.js);
 
 var tweet = new new Twitter(keys);
 
@@ -12,3 +12,23 @@ var argument = process.argv[2];
 var value = process.argv[3];
 var dataText = process.argv[4];
 
+// Twitter parameters with user account
+var parameters = {
+	"screen_name": "KeliChau18",
+	"count": 20
+}
+
+// Twitter if statement
+if(argument === "my-tweets"){
+	twit.get('statuses/user_timeline', parameters, gotData);
+	function gotData (error, data, response){
+		var tweets = data; // object
+		for (var i = 0; i < tweets.length; i++){
+			console.log(tweets[i].text);
+			console.log(tweets[i].created_at);
+		}
+	};
+	outputText();
+};
+
+// OMDB if statement
